62ce5d627e5e984fb03b0cfbfbc1ccd196bea64c
Jose Miguel <jose.miguel.usp@gmail.com>
2019-04-11 23:55:55 -0400

    commit changes docker files

--- java/move-kafka-hdfs/src/main/scala/com/demo/aviation/MoveKafakaHDFS.scala
+++ java/move-kafka-hdfs/src/main/scala/com/demo/aviation/MoveKafakaHDFS.scala
@@ -17,7 +17,7 @@ object MoveKafakaHDFS extends App {
   val conf = new SparkConf().setAppName("Demo Aviation Data").setMaster("spark://spark-master-demo:7077").set("spark.streaming.kafka.maxRatePerPartition", "3000")
 
   // val conf = new SparkConf().setAppName("appName").setMaster("local[*]").set("spark.streaming.kafka.maxRatePerPartition", "100")
-  val streamingContext = new StreamingContext(conf, Seconds(300))
+  val streamingContext = new StreamingContext(conf, Seconds(180))
   // val streamingContext = new StreamingContext(conf, Seconds(10))
   val spark = SparkSession.builder.config(conf).getOrCreate()
   import spark.implicits._
@@ -53,21 +53,27 @@ object MoveKafakaHDFS extends App {
 
       val timeTimestamp = System.currentTimeMillis/1000;
 
-      val df_date = df.withColumn("year_partition", lit(year(from_unixtime(unix_timestamp()))))
-        .withColumn("month_partition", lit(month(from_unixtime(unix_timestamp()))))
-        .withColumn("day_partition", lit(dayofmonth(from_unixtime(unix_timestamp()))))
-        .withColumn("hour_partition", lit(hour(from_unixtime(unix_timestamp()))))
-        .withColumn("year", $"year_partition")
-        .withColumn("month", $"month_partition")
-        .withColumn("day", $"day_partition")
-        .withColumn("hour", $"hour_partition")
+      val df_date = df.select(
+        "ADC_AOA_NORMALIZED",
+        "ADC_LH_IAS",
+        "ADC_RH_IAS",
+        "ADC_STBY_IAS",
+        "APU_BLEED_SW3",
+        "APU_MASTER_SW2")
+//        .withColumn("year_partition", lit(year(from_unixtime(unix_timestamp()))))
+//        .withColumn("month_partition", lit(month(from_unixtime(unix_timestamp()))))
+//        .withColumn("day_partition", lit(dayofmonth(from_unixtime(unix_timestamp()))))
+//        .withColumn("hour_partition", lit(hour(from_unixtime(unix_timestamp()))))
+//        .withColumn("year", $"year_partition")
+//        .withColumn("month", $"month_partition")
+//        .withColumn("day", $"day_partition")
+//        .withColumn("hour", $"hour_partition")
 
       val df_final = df_date.toDF(df_date.columns map (_.toLowerCase): _*)
 
 //      df_final.show(5)
 
-      df_final.repartition(1).write.partitionBy("year", "month", "day", "hour")
-        .mode("append").format("orc").save("hdfs://namenode-demo:9000/demo-aviation/sensorRawData")
+      df_final.write.mode("append").format("csv").save("hdfs://namenode-demo:9000/demo-aviation//test/sensorRawData")
 
     }
 
